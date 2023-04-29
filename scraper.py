0886751964cbbb8a8eec459ccb17d9f7827e0005
awen5 <awen5@uci.edu>
2023-04-29 00:45:00 -0700

    included set of discovered_urls

--- scraper.py
+++ scraper.py
@@ -2,7 +2,8 @@ import re
 from lxml import etree
 from io import StringIO
 from urllib.parse import urlparse, urlunparse, urldefrag, urljoin
-
+import matplotlib.pyplot as plt
+from collections import Counter
 
 
 
@@ -11,6 +12,7 @@ from utils.download import download
 # dictionary to contain subdomains of ics.uci.edu and number of unique pages in that subdomain
 subdomain_pages = {}
 set_subdomain_pages = set()
+discovered_set_pages = set()
 
 def scraper(url:str, resp) -> list:
     links = extract_next_links(url, resp)
@@ -33,19 +35,47 @@ def extract_next_links(url, resp):
     urls = list()
     try:
         # crawling URL only if status code is 200
+        # print(resp.status)
         if resp.status == 200:
             # parsing website
+
             parser = etree.HTMLParser()
             tree = etree.HTML(resp.raw_response.content, parser)
+
+
             # tree = etree.parse(StringIO(resp.raw_response.content), root)
             # result = etree.tostring(tree.getroot(), pretty_print=True, method="html")
             # parsed_url = urlparse(etree.tostring(tree))
 
+            # num_tags = Counter(x.tag for x in tree.iter())
+            # cum_tag = {}
+            # num_cum = 0
+            #
+            # for a, b in num_tags.most_common():
+            #     num_cum += b
+            #     cum_tag[a] = num_cum
+            #
+            # avg = num_cum / len(num_tags)
+            # spike = {a: b for a, b in cum_tag.items()} # if b > avg * 2}
+            #
+            # list_block = []
+            # print("SPIKE_LIST", spike, "AVERAGE", avg)
+            # for i in spike.keyS():
+            #     print("SPIKE", i)
+            #     # block = tree.xpath('//' + i)
+            #     # print("BLOCK", block)
+            #     # if block:
+            #     #     list_block.append(block)
+            # print("list_block", list_block)
+
             # getting only the hyperlinks
             for link in tree.xpath('//a | //img'):
+            # for link in list_block:
+            #     print("LINK", link)
                 relative_url = link.get('href') or link.get('src')
-                # relativeurl_src = link.get('src')
-                # print(relativeurl_src)
+
+
+                # print("RELATIVE_URL", relative_url, "URL", url)
 
 
 
@@ -53,10 +83,25 @@ def extract_next_links(url, resp):
                 if relative_url and relative_url.startswith('#'):
                     # print(relative_url)
                     continue
-
-
+                if relative_url is None:
+                    continue
 
                 parsed_url = urlparse(relative_url)
+                # print("PARSED_URL", parsed_url)
+                if parsed_url.path == '/':
+                    continue
+
+                path = parsed_url.path.split('/')
+
+                if len(path) > 10:
+                    continue
+                # print("PATH", path)
+                temp_set = set()
+                for i in path[1:]:
+                    print("i", i)
+                    if i in temp_set:
+                        continue
+                    temp_set.add(i)
 
                 # converting relative urls to absolute URL
                 if parsed_url.netloc == '' and parsed_url.scheme == '':
@@ -69,6 +114,8 @@ def extract_next_links(url, resp):
                 if parsed_url.scheme and parsed_url.netloc:
                     # removing fragments from URL
                     defrag, _ = urldefrag(relative_url)
+                    if defrag in set_subdomain_pages:
+                        continue
                     # looking for subdomains of the domain ics.uci.edu
                     if "ics.uci.edu" in parsed_url.netloc:
                         split_list = parsed_url.netloc.split(".")
@@ -82,6 +129,7 @@ def extract_next_links(url, resp):
                                     subdomain_pages[split_list[0]] = 1
                             set_subdomain_pages.add(defrag)
                         # print(defrag, "NETLOC", parsed_url.netloc, "SPLIT", parsed_url.netloc.split(".")[0])
+                    # print("DEFRAG", defrag)
                     urls.append(defrag)
 
                 # converting relative urls to absolute URL
@@ -102,21 +150,21 @@ def extract_next_links(url, resp):
             urls = list(dict.fromkeys(urls))
             # print(urls)
             # print(len(urls))
-            print(set_subdomain_pages)
+            # print(set_subdomain_pages)
             # subdomain_pages = sorted(subdomain_pages.items(), key = lambda x: (x[1],x[0]))
 
-            print(subdomain_pages)
+            # print(subdomain_pages)
 
             # print(etree.tostring(tree))
-            print("\n\n\n")
+            # print("\n\n\n")
             # return urls
             # return list()
             return urls
         else:
-            print(url, resp.error)
+            # print(url, resp.error)
             return list()
     except:
-        print("TRIGGERED", url, resp.status)
+        # print("TRIGGERED", url, resp.status, resp.error)
         pass
 
 
@@ -125,11 +173,16 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    if url in discovered_set_pages:
+        return False
+    else:
+        discovered_set_pages.add(url)
     try:
 
         parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
-            return False
+
+        # if parsed.scheme not in set(["http", "https"]):
+        #     return False
         if not re.match(
                 r".+[(\.ics\.uci\.edu)|(\.cs\.uci\.edu)|(\.informatics\.uci\.edu)|(\.stat\.uci\.edu)]", parsed.netloc):
             return False
